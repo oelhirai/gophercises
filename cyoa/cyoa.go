d9e77adf877955772e59738cbb576a4d6cf68d25
oelhirai <oelhiraika@gmail.com>
2018-09-14 08:36:13 -0700

    Added css for styling

--- cyoa/cyoa.go
+++ cyoa/cyoa.go
@@ -22,20 +22,62 @@ var defaultHandlerTmpl = `
 		<title>Create Your Own Adventure</title>
 	</head>
 	<body>
-		<h1>{{.Title}}</h1>
-		{{range .Paragraphs}}
-			<p>{{.}}</p>
-		{{end}}
-		{{ if .Options}}
-			<ul>
-			{{range .Options}}
-				<li><a href="{{.NextArc}}">{{.Text}}</a></li>
+		<section class="page">
+			<h1>{{.Title}}</h1>
+			{{range .Paragraphs}}
+				<p>{{.}}</p>
 			{{end}}
-			</ul>
-		{{ else }}
-			<a href="intro">Restart Game</a>
-		{{end}}
+			{{ if .Options}}
+				<ul>
+				{{range .Options}}
+					<li><a href="{{.NextArc}}">{{.Text}}</a></li>
+				{{end}}
+				</ul>
+			{{ else }}
+				<a href="intro">Restart Game</a>
+			{{end}}
+		</section>
 	</body>
+	<style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FFFCF6;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #777;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: none;
+        color: #6295b5;
+      }
+      a:active,
+      a:hover {
+        color: #7792a2;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
 </html>
 `
 
