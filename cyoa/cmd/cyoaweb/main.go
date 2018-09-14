82452639340263cdea4b9642176a59e0bf30d08d
oelhirai <oelhiraika@gmail.com>
2018-09-14 08:08:46 -0700

    Brought template and http logic over to cyoa package
    List of changes:
    - Created a new struct adhering to http.Handler interfact with a story property
    - Moves JSON decoding logic to package for reusability
    	- Learned unmarshall/marshall is great for buffers (ioutil.ReadAll(r io.Reader))
    	- Can decode file directtly by creating a json.NewDecoder(r) with reader 'r'
    - template.Must is helpful in validating templates before use
    - Learned Log.Fatal(http.Listen...) is a common convention

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -3,24 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
-	"encoding/json"
-	"html/template"
 	"flag"
 	"net/http"
-	"strings"
+	"log"
 	"github.com/oelhirai/gophercises/cyoa"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
 	filename := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *filename)
 
 	storyMap := parseJson(*filename)
-	handler := createStoryHandler(storyMap)
+	handler := cyoa.NewHandler(storyMap)
 
-	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", handler)
+	fmt.Printf("Starting the server on %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), handler))
 }
 
 func parseJson(name string) cyoa.Story {
@@ -29,49 +28,10 @@ func parseJson(name string) cyoa.Story {
 		panic(err)
 	}
 
-	d := json.NewDecoder(jsonFile)
-	var story cyoa.Story
-	if err := d.Decode(&story); err != nil {
+	story, err := cyoa.JsonStory(jsonFile)
+	if err != nil {
 		panic(err)
 	}
 
 	return story
-}
-
-func createStoryHandler(storyMap cyoa.Story) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		path := strings.TrimLeft(r.URL.Path, "/")
-		t := template.New("Arc Template")
-		t, _ = t.Parse(`
-		<html>
-		<head>
-			<title>Create Your Own Adventure!</title>
-		</head>
-		<body>
-			<h2>{{.Title}}</h2>
-			<p>
-				{{range .Paragraphs}}
-				  {{.}}
-				{{end}}
-			</p>
-
-			{{ if .Options}}
-				<h3>Make a choice...</h3>
-				{{range .Options}}
-					<a href="{{.NextArc}}">{{.NextArc}}</a> : {{.Text}}
-					<br>
-				{{end}}
-			{{ else }}
-				<a href="intro">Restart Game</a>
-			{{end}}
-		</body>
-		</html>
-		`)
-
-		if story, ok := storyMap[path]; ok {
-			t.Execute(w, story)
-		} else {
-			t.Execute(w, storyMap["intro"])
-		}
-	})
 }
--- cyoa/cyoa.go
+++ cyoa/cyoa.go
@@ -1,5 +1,72 @@
 package cyoa
 
+import (
+	"io"
+	"encoding/json"
+	"net/http"
+	"strings"
+	"html/template"
+)
+
+func init(){
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf8">
+		<title>Create Your Own Adventure</title>
+	</head>
+	<body>
+		<h1>{{.Title}}</h1>
+		{{range .Paragraphs}}
+			<p>{{.}}</p>
+		{{end}}
+		{{ if .Options}}
+			<ul>
+			{{range .Options}}
+				<li><a href="{{.NextArc}}">{{.Text}}</a></li>
+			{{end}}
+			</ul>
+		{{ else }}
+			<a href="intro">Restart Game</a>
+		{{end}}
+	</body>
+</html>
+`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimLeft(r.URL.Path, "/")
+	if story, ok := h.s[path]; ok {
+		tpl.Execute(w, story)
+	} else {
+		tpl.Execute(w, h.s["intro"])
+	}
+}
+
+
+
+func JsonStory(r io.Reader) (Story, error){
+	d := json.NewDecoder(r)
+	var story Story
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
+
 type Story map[string]Arc
 
 type Arc struct {
